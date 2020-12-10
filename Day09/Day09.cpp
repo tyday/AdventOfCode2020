f7bdce0ce0477322f856cc411294531ed3244ec1
Ty Day <tyrday@gmail.com>
2020-12-10 05:15:24 -0500

    can't get cpp Day9 to work

--- Day09/Day09.cpp
+++ Day09/Day09.cpp
@@ -1,21 +1,24 @@
+// Does not work
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 
-std::vector <int> getData(std::string fileName);
-int findWeakPoint(std::vector<int> data, int preambleLength);
+std::vector <long long> getData(std::string fileName);
+long long findWeakPoint(std::vector<long long> &data, long long preambleLength);
+bool cantFindMatch(std::vector <long long> &data, long long target);
 
 int main(){
 
-    std::vector <int> data = getData("/home/pi/Programming/AdventOfCode/2020/Day09/input.txt");
-    int response = findWeakPoint(data,25);
+    std::vector <long long> data = getData("/home/pi/Programming/AdventOfCode/2020/Day09/input.txt");
+    long long response = findWeakPoint(data,25);
     std::cout << "Answer: " << response << "\n";
 }
 
-std::vector <int> getData(std::string fileName){
-    std::fstream myfile;
-    std::vector<int> data{};
+std::vector <long long> getData(std::string fileName){
+    std::ifstream myfile;
+    std::vector<long long> data{};
     myfile.open(fileName, std::ios::in);
     if(!myfile){
         std::cout << "Cannot find file: " << fileName << "\n";
@@ -23,17 +26,17 @@ std::vector <int> getData(std::string fileName){
     }
     std::string line;
     while(getline(myfile, line)){
-        data.push_back(std::stoi(line));
+        data.push_back(std::stoll(line));
     }
     std::cout << "File loaded: " << fileName << "\n";
     myfile.close();
     return data;
 }
 
-bool cantFindMatch(std::vector <int> data, int target){
+bool cantFindMatch(std::vector <long long> &data, long long target){
     for(int i = 0; i < data.size(); i++){
-        std::vector <int> sub_data = data;
-        int val = sub_data[i];
+        std::vector <long long> sub_data = data;
+        long long val = sub_data[i];
         sub_data.erase(sub_data.begin()+i);
         for(int j=0; j<sub_data.size(); j++){
             if (val +sub_data[j] == target)
@@ -43,11 +46,11 @@ bool cantFindMatch(std::vector <int> data, int target){
     return true;
 }
 
-int findWeakPoint(std::vector<int> data, int preambleLength){
+long long findWeakPoint(std::vector<long long> &data, long long preambleLength){
     int i = preambleLength;
     while( i <data.size()){
-        int target_value = data[i];
-        std::vector<int> sub_list{};
+        long long target_value = data[i];
+        std::vector<long long> sub_list{};
         // sub_list = data[i-preamble_length:i]
         for(int j=i-preambleLength; j<i; j++){
             sub_list.push_back(data[j]);
