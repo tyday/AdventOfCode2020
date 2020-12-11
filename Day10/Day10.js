ceea43cfe309917ca76fae00292478dc555a0751
Ty Day <tyrday@gmail.com>
2020-12-10 22:14:54 -0500

    Part two js

--- Day10/Day10.js
+++ Day10/Day10.js
@@ -1,6 +1,42 @@
 var fs = require("fs");
 
+let adapter_dict = {}
+let search_adapters = (index) => {
+    if (adapter_dict[index] !== undefined){
+        return adapter_dict[index];
+    }
+    else if (index === data.length-1){
+        // Success! We've hit the last node
+        return 1;
+    }
+    else if (index >= data.length){
+        // We've gone too far
+        return 0;
+    }
+    else {
+        // We take the next three indexes on the list
+        let a = index +1;
+        let b = index +2;
+        let c = index +3;
+        current_targets = [data[index] +1,data[index] +2,data[index] +3]
+        let val_a = 0;
+        let val_b = 0;
+        let val_c = 0;
 
+        if(current_targets.includes(data[a])){
+            val_a = search_adapters(a);
+        }
+        if((index+2 < data.length) && (current_targets.includes(data[b]))){
+            val_b = search_adapters(a);
+        }
+        if((index+3 < data.length) && (current_targets.includes(data[c]))){
+            val_c = search_adapters(a);
+        }
+        let current_values = val_a + val_b + val_c;
+        adapter_dict[index] = current_values;
+        return current_values;
+    }
+}
 
  
 
@@ -8,7 +44,7 @@ var fs = require("fs");
 
 
 
-var data = fs.readFileSync('test_input.txt');
+var data = fs.readFileSync('/home/pi/Programming/AdventOfCode/2020/Day10/test_input.txt');
 data = data.toString().split('\n')
 data.push('0');
 data = data.map(x => Number(x));
@@ -32,3 +68,9 @@ data.forEach(adapter =>{
 data.push(data.slice(-1)[0]+ 3);
 console.log(data)
 console.log(volt_dict)
+
+let start = Date.now();
+console.log(search_adapters(0));
+console.log(adapter_dict);
+let time = Date.now() - start;
+console.log(time);
